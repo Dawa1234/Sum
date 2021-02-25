760a62b11506a07e8f28e6626505485dce3cbc41
khangdu <donotdare>
2021-02-25 09:58:58 +0545

    committed

--- questions/questiontwo.py
+++ questions/questiontwo.py
@@ -1,4 +1,13 @@
-a = 2
-b = 2
-c = a + b
-print(c)
+# Name and age of a person.
+
+name = input("Enter the name:")
+age = int(input("Enter the age:"))
+
+print(name, "is the name and " + str(age) + " is the age")
+print("%s is the name and %s is the age" % (name, age))
+print("{} is the name and {} is the age".format(name, age))
+print(f"{name} is the name and {age} is the age")
+
+
+
+
