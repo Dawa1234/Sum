7f1c099eb189f151a3c5d6bb9e312c9c5e238b0b
khangdu <donotdare>
2021-02-25 09:21:11 +0545

    Sum of Numbers

---
+++ labone/sumofapple.py
@@ -0,0 +1,8 @@
+#Write a program that takes three numbers and print thier sum.Every number is given in seperate line.
+num1 = int(input("Enter the first value:"))
+num2 = int(input("Enter the second value:"))
+num3 = int(input("Enter the third value:"))
+sum = num1 + num2 + num3
+print ("The total sum is", sum)
+
+
