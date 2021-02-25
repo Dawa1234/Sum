ae1d274d0dc0ca894ff35e8844cd1b7ca5902112
khangdu <donotdare>
2021-02-25 10:13:10 +0545

    questions

---
+++ questions/questionfour.py
@@ -0,0 +1,5 @@
+# Given the integer N - the number of minutes that is passed since midnight - how many hours and minutes displayed
+# on the 24th digital clock.
+
+
+
---
+++ questions/questiontwwo.py
@@ -0,0 +1,7 @@
+# Write a program that reads the length of base and height of right-angled-triangle and print the area.Every no.
+# is given on seperate line.
+
+length = int(input("Enter the length of the base:"))
+height = int(input("Enter the height of the triangle:"))
+area = (1/2) * length * height
+print(f"The area of the triangle is {area}")
