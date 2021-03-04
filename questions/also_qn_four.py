41c59a2856fc6f9a35e7124a947fc13712d0831e
khangdu <donotdare>
2021-03-04 10:25:25 +0545

    Committed /  Changed

--- questions/also_qn_four.py
+++ questions/also_qn_four.py
@@ -7,8 +7,15 @@ hours = (N//60)
 minute = (N%60)
 print("The hour is %s" %(hours))
 print(f"The minute is {minute}")
-for i  in range(3):
+if N < 720 :
     print("The time right now is {}:{}am".format(hours, minute))
+
+elif N == 720 :
+    print("The time right now is {}:{}pm".format(hours, minute))
+
+else:
+    print("The time right now is {}:{}pm".format(hours, minute))
+
 
 
 
--- questions/apples.py
+++ questions/apples.py
@@ -1,2 +1,9 @@
 # N students take k apples and distributes them among each others evenly. The remaining (the undivisible) part
 # reamains in the basket. How many apples will each single student get? How many apples will remain in the basket?
+
+apple = int(input("Enter the no. of apple: "))
+student = int(input("Enter the no. of students: "))
+each_students = apple//student
+remaining = apple%student
+print(f"Each student gets {each_students} apple")
+print("The remaining apple in the basket is", remaining)
