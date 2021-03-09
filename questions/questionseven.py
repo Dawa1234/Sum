e06201510c10b287ab7f228afcfbae1e2e1f5f9a
khangdu <donotdare>
2021-03-09 11:59:05 +0545

    Bus and the boy

--- questions/questionseven.py
+++ questions/questionseven.py
@@ -3,23 +3,22 @@
 # run the next two at 15 m/h; before jogging the last at 7 m/h again. Will this be quicker or slower then bus?
 
 
-distance = 4 * 1.6 * 1000
+distance = 4
 speed_bus = 25 /(60 * 60)
-time_taken = ((((distance/speed_bus) + (2 * 60))//60)//60)//24
-print(f"The bus will take {time_taken} days to reach university")
+time_taken = ((distance/speed_bus) + (2 * 60))//60
+print(f"The bus will take around {time_taken} min to reach university")
 
 speed_student1 = 7/(60 * 60)
-time_taken1 = (1.6 * 1000)/ speed_student1
+time_taken1 = 1/ speed_student1
 speed_student2 = 15/(60 * 60)
-time_taken2 = (2 * 1.6 * 1000)/speed_student2
+time_taken2 = 2/speed_student2
 speed_student3 = 7/(60 * 60)
-time_taken3 = (1.6 * 1000)/ speed_student3
+time_taken3 = 1/ speed_student3
 
-TotalTimeTaken = time_taken1 + time_taken2 + time_taken3
-TotalTimeTaken_day = (((TotalTimeTaken//60)//60)//24)
-print(f"It takes {TotalTimeTaken_day}days for the boy to reach the university")
+TotalTimeTaken = (time_taken1 + time_taken2 + time_taken3)//60
+print(f"It takes around {TotalTimeTaken} min for the boy to reach the university")
 
-if time_taken < TotalTimeTaken_day :
+if time_taken < TotalTimeTaken :
     print("The boy will reach slower than bus")
 else:
     print("The boy will reach faster than bus.")
