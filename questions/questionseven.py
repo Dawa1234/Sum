085e0be2ebdc26cba80d7666e4783e5639b02181
khangdu <donotdare>
2021-03-09 20:52:57 +0545

    Distance,bus,boy

--- questions/questionseven.py
+++ questions/questionseven.py
@@ -3,25 +3,25 @@
 # run the next two at 15 m/h; before jogging the last at 7 m/h again. Will this be quicker or slower then bus?
 
 
-distance = 4
-speed_bus = 25 /(60 * 60)
-time_taken = ((distance/speed_bus) + (2 * 60))//60
-print(f"The bus will take around {time_taken} min to reach university")
+distance = 4 *1.6 * 1000
+speed_bus = (25*1.6 * 1000)/(60 *60)
+time_taken = ((distance/speed_bus) + (20 * 60))//60
+print(f"The bus will take around {time_taken} min to reach university.")
 
-speed_student1 = 7/(60 * 60)
-time_taken1 = 1/ speed_student1
-speed_student2 = 15/(60 * 60)
-time_taken2 = 2/speed_student2
-speed_student3 = 7/(60 * 60)
-time_taken3 = 1/ speed_student3
+speed_student1 = (7*1.6 * 1000)/(60 *60)
+time_taken1 = (1*1.6 * 1000)/ speed_student1
+speed_student2 = (15*1.6 * 1000)/(60 *60)
+time_taken2 = (2*1.6 * 1000)/speed_student2
+speed_student3 = (7*1.6 * 1000)/(60 *60)
+time_taken3 = (1*1.6 * 1000)/ speed_student3
 
-TotalTimeTaken = (time_taken1 + time_taken2 + time_taken3)//60
-print(f"It takes around {TotalTimeTaken} min for the boy to reach the university")
+TotalTimeTaken = ((time_taken1 + time_taken2 + time_taken3))//60
+print(f"It takes around {TotalTimeTaken} min for the boy to reach the university.")
 
 if time_taken < TotalTimeTaken :
-    print("The boy will reach slower than bus")
+    print("Therefore, The boy will reach slower than bus")
 else:
-    print("The boy will reach faster than bus.")
+    print("Therefore, The boy will reach faster than bus.")
 
 
 
