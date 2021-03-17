283f46f0855efd100d0c3b72a636038d55fcec27
khangdu <donotdare>
2021-03-17 09:27:02 +0545

    check git

--- questions/five.py
+++ questions/five.py
@@ -10,14 +10,14 @@ print(f"{NumberOfDesk} desks are needed for a first class.")
  print("{} desks are needed for a second class.".format(Number_OfDesk))
  print("%s desks are needed for a third class." %(Number_Of_Desk))
 
- reamaining_desk1 = student1 % 2
- reamaining_desk2 = student2 % 2
- reamaining_desk3 = student3 % 2
+reamaining_desk1 = student1 % 2
+reamaining_desk2 = student2 % 2
+reamaining_desk3 = student3 % 2
 
- print(f"{reamaining_desk1} desks are remained from first calss")
- print(f"{reamaining_desk2} desks are remained from second calss")
- print(f"{reamaining_desk3} desks are remained from third calss")
+print(f"{reamaining_desk1} desks are remained from first calss")
+print(f"{reamaining_desk2} desks are remained from second calss")
+print(f"{reamaining_desk3} desks are remained from third calss")
 
- total_desk_purchase = Number_Of_Desk + Number_OfDesk + NumberOfDesk + reamaining_desk1 + reamaining_desk2 + reamaining_desk3
+total_desk_purchase = Number_Of_Desk + Number_OfDesk + NumberOfDesk + reamaining_desk1 + reamaining_desk2 + reamaining_desk3
 
- print(f"{total_desk_purchase} desks should be purchased for total classes")
+print(f"{total_desk_purchase} desks should be purchased for total classes")
--- questions/three.py
+++ questions/three.py
@@ -1,15 +1,15 @@
 #How many apples through the basket were distributed to how many persons and remained in the basket.Calculated.
 
- apple_inbasket = int(input("Enter the no. of apple:"))
- total_people = int(input("Enter the no. of people:"))
- people_gotten = int(input("Enter the no. of apple distributed:"))
- remaining_apple = apple_inbasket - people_gotten
- total_people_got = people_gotten/total_people
- #Applying Condition for apple.
- if remaining_apple < 1 : print ("The is no remaining apple in the basket")
- elif remaining_apple <= 1 :print("The remaining apple in the basket is", remaining_apple)
- else : print("The remaining apples in the basket are", remaining_apple)
- #Applying Condition for people.
- if total_people_got <1 : print ("{} apple was distributed to each person.".format(total_people_got))
- elif total_people_got <= 1:print("%s apples was distributed to each person."%(total_people_got))
- else : print(" {} apples were distributed to each person.".format(total_people_got))
+apple_inbasket = int(input("Enter the no. of apple:"))
+total_people = int(input("Enter the no. of people:"))
+people_gotten = int(input("Enter the no. of apple distributed:"))
+remaining_apple = apple_inbasket - people_gotten
+total_people_got = people_gotten/total_people
+#Applying Condition for apple.
+if remaining_apple < 1 : print ("The is no remaining apple in the basket")
+elif remaining_apple <= 1 :print("The remaining apple in the basket is", remaining_apple)
+else : print("The remaining apples in the basket are", remaining_apple)
+#Applying Condition for people.
+if total_people_got <1 : print ("{} apple was distributed to each person.".format(total_people_got))
+elif total_people_got <= 1:print("%s apples was distributed to each person."%(total_people_got))
+else : print(" {} apples were distributed to each person.".format(total_people_got))
